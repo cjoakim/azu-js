3e37d7db6d57c8e9eeb901db96c9ae2517f24240
Chris Joakim <christopher.joakim@gmail.com>
2023-09-23 06:19:34 -0400

    bump version to 0.9.1

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,7 +2,7 @@
 export class Constants {
     
     public static LIB_NAME         : string = 'azu-js';
-    public static LIB_VERSION      : string = '0.9.0';
+    public static LIB_VERSION      : string = '0.9.1';
     public static LIB_AUTHOR       : string = 'Chris Joakim';
     public static LIB_LICENSE      : string = 'MIT';
 
