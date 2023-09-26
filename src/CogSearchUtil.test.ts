e53265b1bee147d7d08a64d5b562c1ff241ed2e7
Chris Joakim <christopher.joakim@gmail.com>
2023-09-26 15:13:27 -0400

    CogSearchUtil - wip

--- src/CogSearchUtil.test.ts
+++ src/CogSearchUtil.test.ts
@@ -2,6 +2,8 @@
 // Unit tests for class CogSearchUtil
 // Chris Joakim, Microsoft, 2023
 
+// npm test --testPathPattern CogSearchUtil
+
 import fs from "fs";
 import path from "path";
 import util from "util";
@@ -10,23 +12,51 @@ import { CogSearchUtil } from "./CogSearchUtil";
 import { Config } from "./Config";
 import { FileUtil } from "./FileUtil";
 
-let searchNameEnvVar : string = Config.lookupEnvVarName('ENV_SEARCH_NAME');
-let adminKeyEnvVar   : string = Config.lookupEnvVarName('ENV_SEARCH_ADMIN_KEY');
-let queryKeyEnvVar   : string = Config.lookupEnvVarName('ENV_SEARCH_QUERY_KEY');
-let searchUtil : CogSearchUtil = null;
+let acctUriEnvVar      : string = Config.lookupEnvVarName('ENV_SEARCH_URL');
+let acctNameEnvVar     : string = Config.lookupEnvVarName('ENV_SEARCH_NAME');
+let acctAdminKeyEnvVar : string = Config.lookupEnvVarName('ENV_SEARCH_ADMIN_KEY');
+let acctQueryKeyEnvVar : string = Config.lookupEnvVarName('ENV_SEARCH_QUERY_KEY');
+
+let apiVersion : string = '2023-07-01-Preview';
+let su : CogSearchUtil = null;
 
 beforeAll(() => {
-    searchUtil = initCogSearchUtil();
+    su = initCogSearchUtil();
 });
 
 function initCogSearchUtil() : CogSearchUtil {
-    return null;  //new CogSearchUtil(acctUriEnvVar, acctKeyEnvVar);
+    return new CogSearchUtil(
+        acctUriEnvVar,
+        acctNameEnvVar,
+        acctAdminKeyEnvVar,
+        acctQueryKeyEnvVar,
+        apiVersion);
 }
 
 function epochTime() : number {
     return Date.now().valueOf();
 }
 
-test("CogSearchUtil: constructor", async () => {
+test("CogSearchUtil: constructor and headers", async () => {
     expect(true).toBe(true);
+
+    expect(su.acctName.length).toBeGreaterThan(6);
+    expect(su.acctURI.length).toBeGreaterThan(6);
+    expect(su.acctURI).toContain('https://');
+    expect(su.acctURI).toContain('.search.windows.net');
+    expect(su.apiVersion).toBe(apiVersion);
+
+    let ah = su.adminHeaders;
+    expect(ah['Content-Type']).toBe('application/json');
+    expect(ah['api-key'].length).toBeGreaterThan(30);
+
+    let qh = su.queryHeaders;
+    expect(qh['Content-Type']).toBe('application/json');
+    expect(qh['api-key'].length).toBeGreaterThan(30);
+});
+
+test("CogSearchUtil: url methods", async () => {
+
+    expect(su.listIndexesUrl()).toContain('.search.windows.net/indexes?api-version=2023-07-01-Preview');
+
 });
--- src/CogSearchUtil.ts
+++ src/CogSearchUtil.ts
@@ -17,22 +17,117 @@ import {
 
 export class CogSearchUtil {
 
+    acctURI      : string = null;
+    acctName     : string = null;
+    adminHeaders : Object = null;
+    queryHeaders : Object = null;
+    apiVersion   : string = null;
+
     // Pass in the names of the environment variables that contain the
-    // Azure Cosmos DB account URI and Key.
+    // configuration values.
     constructor(
-        acctUriEnvVar : string,
-        acctKeyEnvVar : string,
+        acctUriEnvVar  : string,
+        acctNameEnvVar : string,
+        acctAdminKeyEnvVar : string,
+        acctQueryKeyEnvVar : string,
+        apiVersion : string,
         verbose?: boolean) {
 
         try {
-
+            this.acctURI = process.env[acctUriEnvVar] as string;
+            this.acctName = process.env[acctNameEnvVar] as string;
+            this.apiVersion = apiVersion;
+            this.adminHeaders = this.buildHttpHeader(acctAdminKeyEnvVar);
+            this.queryHeaders = this.buildHttpHeader(acctQueryKeyEnvVar);
         }
         catch (error) {
             console.log(error);
         }
     }
 
+    private buildHttpHeader(keyEnvVarName) : Object {
+        let headers = {};
+        headers['Content-Type'] = 'application/json';
+        headers['api-key'] = process.env['' + keyEnvVarName] as string;
+        return headers;
+    }
+
     dispose() : void {
         return;
     }
+
+    // URL methods below:
+
+
+    listIndexesUrl() : string {
+        return util.format("%s/indexes?api-version=%s", this.acctURI, this.apiVersion);
+    }
+
+    // list_indexers_url(): string {
+    //     return '{}/indexers?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // list_datasources_url(): string {
+    //     return '{}/datasources?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // list_skillsets_url(): string {
+    //     return '{}/skillsets?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // get_index_url(self, name):
+    //     return '{}/indexes/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // get_indexer_url(self, name):
+    //     return '{}/indexers/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // get_indexer_status_url(self, name):
+    //     return '{}/indexers/{}/status?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // get_datasource_url(self, name):
+    //     return '{}/datasources/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // get_skillset_url(self, name):
+    //     return '{}/skillsets/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // create_index_url(): string {
+    //     return '{}/indexes?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // modify_index_url(self, name):
+    //     return '{}/indexes/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // create_indexer_url(): string {
+    //     return '{}/indexers?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // modify_indexer_url(self, name):
+    //     return '{}/indexers/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // reset_indexer_url(self, name):
+    //     return '{}/indexers/{}/reset?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // run_indexer_url(self, name):
+    //     return '{}/indexers/{}/run?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // create_datasource_url(): string {
+    //     return '{}/datasources?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // modify_datasource_url(self, name):
+    //     return '{}/datasources/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // create_synmap_url(): string {
+    //     return '{}/synonymmaps?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // modify_synmap_url(self, name):
+    //     return '{}/synonymmaps/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // create_skillset_url(): string {
+    //     return '{}/skillsets?api-version={}'.format(self.search_url, self.search_api_version)
+
+    // modify_skillset_url(self, name):
+    //     return '{}/skillsets/{}?api-version={}'.format(self.search_url, name, self.search_api_version)
+
+    // search_index_url(self, idx_name):
+    //     return '{}/indexes/{}/docs/search?api-version={}'.format(self.search_url, idx_name, self.search_api_version)
+
+    // lookup_doc_url(self, index_name, doc_key):
+    //     return '{}/indexes/{}/docs/{}?api-version={}'.format(self.search_url, index_name, doc_key, self.search_api_version)
+
+
 }
