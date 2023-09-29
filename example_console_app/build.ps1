27a5f3600ddb9c5a636500ce47bdc2546061aeee
Chris Joakim <christopher.joakim@gmail.com>
2023-09-29 09:03:20 -0400

    Copied apps/console to example_console_app/

---
+++ example_console_app/build.ps1
@@ -0,0 +1,7 @@
+
+# PowerShell script to compile this app.
+#
+# Chris Joakim, Microsoft, 2023
+
+npm run build
+
---
+++ example_console_app/src/index.ts
@@ -0,0 +1,158 @@
+
+import {
+    BlobUtil,
+    Config,
+    FileUtil,
+    CosmosNoSqlUtil,
+    OpenAiUtil,
+    CogSearchUtil
+} from "azu-js";
+
+import fs from "fs";
+import path from "path";
+import { json } from "stream/consumers";
+import util from "util";
+
+export interface CogSearchResponse {
+    url:    string;
+    method: string;
+    status: number;
+    data:   Object;
+    error:  boolean;
+}
+
+let func = process.argv[2];
+let fu   = new FileUtil();
+
+console.log('========================================');
+console.log(util.format('func: %s', func));
+
+switch (func) {
+    case "config":
+        config();
+        break;
+    case "files":
+        files();
+        break;
+    case "search_setup":
+        searchSetup();
+        break;
+    case "storage":
+        storage();
+        break;
+    case "cosmos_nosql":
+        cosmos_nosql();
+        break;
+    case "cosmos_mongo":
+        cosmos_mongo();
+        break;
+    case "cosmos_pg":
+        cosmos_pg();
+        break;
+    case "embeddings":
+        embeddings();
+        break;
+    default:
+        displayCommandLineExamples();
+        break;
+}
+
+function config() {
+    Config.writeSampleConfigFile();
+    Config.readConfigFile();
+    console.log(JSON.stringify(Config.readConfigFile(), null, 4));
+    console.log(util.format("ENV_OPENAI_URL: %s", Config.lookupEnvVarName('ENV_OPENAI_URL')));
+}
+
+function files() {
+    let epoch : number = epochTime();
+    let outfile : string = util.format('tmp%stest-file-%s.txt', path.sep, epoch);
+    let content : string = util.format("this is a test file.\n%s", epoch);
+
+    console.log('--- writeTextFileSync');
+    let successful : boolean = fu.writeTextFileSync(outfile, content);
+    console.log(successful);
+
+    console.log('--- readTextFileSync');
+    let data : string = fu.readTextFileSync(outfile);
+    console.log(data);
+
+    console.log('--- readJsonObjectFile');
+    let obj = fu.readJsonObjectFile('../../data/usa-states.json');
+    console.log(obj);
+
+    console.log('--- readJsonArrayFile');
+    let airports = fu.readJsonArrayFile('../../data/world-airports-50.json');
+    console.log(airports[0]);
+}
+
+async function searchSetup() {
+
+    let apiVersion : string = '2023-07-01-Preview';
+    let csu : CogSearchUtil = new CogSearchUtil(
+        'AZURE_SEARCH_URL',
+        'AZURE_SEARCH_NAME',
+        'AZURE_SEARCH_ADMIN_KEY',
+        'AZURE_SEARCH_QUERY_KEY',
+        apiVersion,
+        true);
+
+    let resp : CogSearchResponse = await csu.listIndexes();
+    console.log(resp);
+}
+
+async function storage() {
+    let acctNameEnvVar : string = 'AZURE_STORAGE_ACCOUNT';
+    let acctKeyEnvVar : string = 'AZURE_STORAGE_KEY';
+    let bu = new BlobUtil(acctNameEnvVar, acctKeyEnvVar);
+
+    let containersList = await bu.listContainersAsync();
+    console.log(containersList)
+}
+
+async function embeddings() {
+    let acctUriEnvVar : string = Config.lookupEnvVarName('ENV_OPENAI_URL');
+    let acctKeyEnvVar : string = Config.lookupEnvVarName('ENV_OPENAI_KEY');
+    let embDepEnvVar  : string = Config.lookupEnvVarName('ENV_OPENAI_EMB_DEP');
+
+    let oaiUtil = new OpenAiUtil(acctUriEnvVar, acctKeyEnvVar, embDepEnvVar);
+    let fu = new FileUtil();
+    let text = fu.readTextFileSync('../../data/gettysburg-address.txt');
+
+    let e = await oaiUtil.generateEmbeddings([text]);
+    console.log(e);
+    let embeddingsArray = e.data[0]['embedding'];
+    let tokens = e.usage['totalTokens']
+    console.log(util.format(" tokens:\n%s",tokens));
+    console.log(util.format('  embeddings length: %s',embeddingsArray.length));
+    fu.writeTextFileSync('tmp/embeddings_str.txt', text);
+    fu.writeTextFileSync('tmp/embeddings.json', JSON.stringify(e, null, 4));
+}
+
+function cosmos_nosql() {
+    console.log('TODO - implement');
+}
+
+function cosmos_mongo() {
+    console.log('TODO - implement');
+}
+
+function cosmos_pg() {
+    console.log('TODO - implement');
+}
+
+function epochTime() : number {
+    return Date.now().valueOf();
+}
+
+function displayCommandLineExamples() {
+    console.log('');
+    console.log("node .\\dist\\index.js config");
+    console.log("node .\\dist\\index.js files");
+    console.log("node .\\dist\\index.js storage");
+    console.log("node .\\dist\\index.js cosmos_nosql");
+    console.log("node .\\dist\\index.js cosmos_mongo");
+    console.log("node .\\dist\\index.js cosmos_pg");
+    console.log("node .\\dist\\index.js embeddings");
+    console.log('');
+}
